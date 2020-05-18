a785f143a41050bd935d1466632d29308e0cedea
tuananh8497 <tuananh8497@gmail.com>
2020-05-18 21:12:52 +0700

    add SnakeMove, checkApple, doDrawing

--- ST2_T12020_Final/src/SnakeGame/SnakeQueue.java
+++ ST2_T12020_Final/src/SnakeGame/SnakeQueue.java
@@ -1,5 +1,22 @@
 package SnakeGame;
 
-public class SnakeQueue {
+import java.awt.Graphics;
+import java.util.LinkedList;
+import java.util.Queue;
 
+
+public class SnakeQueue {
+  Queue q = new LinkedList<>();
+
+  public void snakeMove() {
+
+  }
+
+  public void checkApple() {
+
+  }
+
+  public void doDrawing(Graphics g) {
+
+  }
 }
