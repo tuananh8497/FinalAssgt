63df0820d3bb5e50ca79e4a7e1d3e89433b7930f
tuananh8497 <tuananh8497@gmail.com>
2020-05-18 15:16:28 +0700

    Added SnakeGame Package

---
+++ ST2_T12020_Final/src/SnakeGame/SnakeDot.java
@@ -0,0 +1,52 @@
+package SnakeGame;
+
+import java.awt.Color;
+
+public class SnakeDot {
+  int x;
+  int y;
+  Color color;
+
+
+  public SnakeDot(int x, int y, Color color) {
+    super();
+    this.x = x;
+    this.y = y;
+    this.color = color;
+  }
+
+  public void asHead() {
+    this.color = Color.red;
+  }
+
+  public void setPos(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public int getX() {
+    return x;
+  }
+
+  public void setX(int x) {
+    this.x = x;
+  }
+
+  public int getY() {
+    return y;
+  }
+
+  public void setY(int y) {
+    this.y = y;
+  }
+
+  public Color getC() {
+    return color;
+  }
+
+  public void setC(Color c) {
+    this.color = color;
+  }
+
+
+}
